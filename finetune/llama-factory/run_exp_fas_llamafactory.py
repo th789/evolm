9fac375b0ebd094e9b51e904864dcc4eadd80350
th789 <30128738+th789@users.noreply.github.com>
2026-05-07 14:34:51 -0400

    ft all models (EXCEPT olmo-140x) on all datasets (cot, lang, safety)

--- finetune/llama-factory/run_exp_fas_llamafactory.py
+++ finetune/llama-factory/run_exp_fas_llamafactory.py
@@ -262,7 +262,7 @@ def run_exp01_finetune_llama():
     #demo w. small alpaca dataset (provided by llamafactory)
     # config_file_paths = ['config_hub/custom_configs/ft_metamathqa/llama-0.5B-10BT-weightdecay0.1-seed42-alpacaendemo.yaml']
 
-    sft_dataset = 'safetymixed2000' 
+    sft_dataset = 'medmcqa' 
     #options: ['metamathqa','medmcqa', pubmedqa', 'mmluprocot', 'race', 'simplescaling' 
     # /// 'hellaswag', 'winogrande', 'piqa', 'arc_easy', 'arc_challenge', 
     # /// 'safety', 'safetymixed300', 'safetymixed500', 'safetymixed1000', 'safetymixed2000'] #safety=old/bad no longer using
@@ -272,9 +272,9 @@ def run_exp01_finetune_llama():
     ### wd=0 models
     config_file_paths = [
         ###llama models
-        f'config_hub/custom_configs/ft_{sft_dataset}/llama-0.5B-10BT-weightdecay0.0-seed42-{sft_dataset}.yaml',
-        f'config_hub/custom_configs/ft_{sft_dataset}/llama-1B-20BT-weightdecay0.0-seed42-{sft_dataset}.yaml',
-        # f'config_hub/custom_configs/ft_{sft_dataset}/llama-4B-80BT-weightdecay0.0-seed42-{sft_dataset}.yaml',
+        # f'config_hub/custom_configs/ft_{sft_dataset}/llama-0.5B-10BT-weightdecay0.0-seed42-{sft_dataset}.yaml',
+        # f'config_hub/custom_configs/ft_{sft_dataset}/llama-1B-20BT-weightdecay0.0-seed42-{sft_dataset}.yaml',
+        f'config_hub/custom_configs/ft_{sft_dataset}/llama-4B-80BT-weightdecay0.0-seed42-{sft_dataset}.yaml',
         ###olmo models
         # f'config_hub/custom_configs/ft_{sft_dataset}/olmo-1B-30BT-weightdecay0.0-{sft_dataset}.yaml',
         # f'config_hub/custom_configs/ft_{sft_dataset}/olmo-1B-210BT-weightdecay0.0-{sft_dataset}.yaml',
@@ -351,10 +351,10 @@ def run_exp01_finetune_llama():
                         # n_nodes='1', n_gpus_a100_80gb='4', n_tasks_per_node='4', cpus_per_task='12', time_hrs='4', memory_gb='64',  #llama1B/llama0.5B/olmo: medmcqa
                         # n_nodes='1', n_gpus_a100_80gb='4', n_tasks_per_node='4', cpus_per_task='12', time_hrs='5', memory_gb='64',  #llama1B/llama0.5B/olmo: pubmedqa, mmluprocot
                         # n_nodes='1', n_gpus_a100_80gb='4', n_tasks_per_node='4', cpus_per_task='12', time_hrs='3', time_mins='30', memory_gb='64',  #olmo: race, simplescaling
-                        n_nodes='1', n_gpus_a100_80gb='4', n_tasks_per_node='4', cpus_per_task='12', time_mins='30', memory_gb='64',  #llama1B/llama0.5B/olmo: hellaswag, winogrande, piqa, arc_easy, arc_challenge, safetymixed300, safetymixed500, safetymixed1000, safetymixed2000 -- very quick<20min
+                        # n_nodes='1', n_gpus_a100_80gb='4', n_tasks_per_node='4', cpus_per_task='12', time_mins='30', memory_gb='64',  #llama1B/llama0.5B/olmo: hellaswag, winogrande, piqa, arc_easy, arc_challenge, safetymixed300, safetymixed500, safetymixed1000, safetymixed2000 -- very quick<20min
                         ###model: 4B llama
-                        # n_nodes='1', n_gpus_a100_80gb='4', n_tasks_per_node='4', cpus_per_task='12', time_hrs='14', memory_gb='64', #llama4B: metamathqa
-                        # n_nodes='1', n_gpus_a100_80gb='4', n_tasks_per_node='4', cpus_per_task='12', time_hrs='8', memory_gb='64', #llama4B: medmcqa
+                        # n_nodes='1', n_gpus_a100_80gb='4', n_tasks_per_node='4', cpus_per_task='12', time_hrs='20', memory_gb='64', #llama4B: metamathqa
+                        n_nodes='1', n_gpus_a100_80gb='4', n_tasks_per_node='4', cpus_per_task='12', time_hrs='15', memory_gb='64', #llama4B: medmcqa
                         # n_nodes='1', n_gpus_a100_80gb='4', n_tasks_per_node='4', cpus_per_task='12', time_hrs='10', memory_gb='64', #llama4B: pubmedqa
                         # n_nodes='1', n_gpus_a100_80gb='4', n_tasks_per_node='4', cpus_per_task='12', time_hrs='6', memory_gb='64', #llama4B: race, simplescaling
                         # n_nodes='1', n_gpus_a100_80gb='4', n_tasks_per_node='4', cpus_per_task='12', time_hrs='9', memory_gb='64', #llama4B: mmluprocot
